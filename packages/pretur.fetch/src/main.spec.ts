206cb0bb8579b3ce45dcd222625a4d0409191862
Ali Taheri <ali.taheri.m@gmail.com>
2016-08-24 13:41:04 +0430

    Partially test pretur.fetch

---
+++ packages/pretur.fetch/src/main.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import fetch from './main';
+
+describe('fetch', () => {
+
+  it('should return node-fetch when in node env', () => {
+    expect(fetch).to.be.equals(require('node-fetch'));
+  });
+
+});
