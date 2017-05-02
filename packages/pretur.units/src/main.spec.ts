00098c423bdae461dd967af69d1577e412760d32
Ali Taheri Moghaddar <ali.taheri.m@gmail.com>
2017-05-02 12:17:29 +0430

    Add units

---
+++ packages/pretur.units/src/main.spec.ts
@@ -0,0 +1,247 @@
+/// <reference types="mocha" />
+
+import { expect } from 'chai';
+import Units from './main';
+
+describe('Units', () => {
+
+  it('should fail to construct if conversions units are invalid', () => {
+    expect(() => {
+      new Units(['g', 'kg'], [
+        ['kg', 'count', 1000],
+      ]);
+    }).to.throw();
+    expect(() => {
+      new Units(['g', 'kg'], [
+        ['g', 'g', 2],
+      ]);
+    }).to.throw();
+    expect(() => {
+      new Units(['g', 'kg'], [
+        ['g', 'kg', 0],
+      ]);
+    }).to.throw();
+  });
+
+  describe('convert', () => {
+
+    it('should perform basic conversion', () => {
+      const units = new Units(['g', 'kg'], [
+        ['kg', 'g', 1000],
+      ]);
+
+      expect(units.convert('g', 'kg', 1)).to.be.equals(0.001);
+      expect(units.convert('g', 'kg', 3)).to.be.equals(0.003);
+      expect(units.convert('g', 'g', 3)).to.be.equals(3);
+      expect(units.convert('kg', 'kg', 2)).to.be.equals(2);
+      expect(units.convert('kg', 'g', 2.5)).to.be.equals(2500);
+    });
+
+    it('should ignore invalid conversions', () => {
+      const units = new Units(['g', 'kg'], [
+        <any>undefined,
+        ['kg', 'g', 1000],
+        <any>false,
+        <any>'',
+        <any>23,
+      ]);
+
+      expect(units.convert('g', 'kg', 1)).to.be.equals(0.001);
+    });
+
+    it('should perform complex conversions', () => {
+      const units = new Units(['1', 'box', 'container', 'g', 'kg'], [
+        ['kg', 'g', 1000],
+        ['1', 'g', 500],
+        ['box', '1', 25],
+        ['container', 'box', 20],
+      ]);
+
+      expect(units.convert('1', 'g', 4)).to.be.equals(2000);
+      expect(units.convert('1', 'kg', 10)).to.be.equals(5);
+      expect(units.convert('kg', '1', 5)).to.be.equals(10);
+      expect(units.convert('kg', '1', 5)).to.be.equals(10);
+      expect(units.convert('container', '1', 2)).to.be.equals(1000);
+      expect(units.convert('container', 'kg', 2)).to.be.equals(500);
+      expect(units.convert('container', 'g', 20)).to.be.equals(5000000);
+      expect(units.convert('kg', 'container', 25)).to.be.equals(0.1);
+    });
+
+  });
+
+  describe('getRatio', () => {
+
+    it('should get ratio where there is one', () => {
+      const units = new Units(['g', 'kg'], [
+        ['kg', 'g', 1000],
+      ]);
+
+      expect(units.getRatio('g', 'kg')).to.be.equals(0.001);
+    });
+
+    it('should return Infinity as ratio where there is none', () => {
+      const units = new Units(['g', 'kg']);
+
+      expect(units.getRatio('g', 'kg')).to.be.equals(Infinity);
+    });
+
+  });
+
+  describe('createConverter', () => {
+
+    it('should return convertor', () => {
+      const units = new Units(['g', 'kg'], [
+        ['kg', 'g', 1000],
+      ]);
+
+      expect(units.createConverter('g', 'kg')!(200)).to.be.equals(0.2);
+    });
+
+    it('should return undefined if there are no ratios', () => {
+      const units = new Units(['g', 'kg']);
+
+      expect(units.createConverter('g', 'kg')).to.be.undefined;
+    });
+
+  });
+
+  describe('getMatrix', () => {
+
+    it('should calculate conversion grid matrix', () => {
+      const units = new Units(['1', 'box', 'container', 'g', 'kg'], [
+        ['kg', 'g', 1000],
+        ['1', 'g', 500],
+        ['box', '1', 25],
+        ['container', 'box', 20],
+      ]);
+
+      const expectedMatrix = {
+        '1': {
+          '1': 1,
+          'box': 0.04,
+          'container': 0.002,
+          'g': 500,
+          'kg': 0.5,
+        },
+        'box': {
+          '1': 25,
+          'box': 1,
+          'container': 0.05,
+          'g': 12500,
+          'kg': 12.5,
+        },
+        'container': {
+          '1': 500,
+          'box': 20,
+          'container': 1,
+          'g': 250000,
+          'kg': 250,
+        },
+        'g': {
+          '1': 0.002,
+          'box': 0.00008,
+          'container': 0.000004,
+          'g': 1,
+          'kg': 0.001,
+        },
+        'kg': {
+          '1': 2,
+          'box': 0.08,
+          'container': 0.004,
+          'g': 1000,
+          'kg': 1,
+        },
+      };
+
+      const matrix = units.getMatrix('nested');
+
+      expect(matrix).to.be.deep.equal(expectedMatrix);
+    });
+
+    it('should calculate conversion grid matrix', () => {
+      const unitNames = ['1', 'box', 'container', 'g', 'kg'];
+      const units = new Units(unitNames, [
+        ['kg', 'g', 1000],
+        ['1', 'g', 500],
+        ['box', '1', 25],
+        ['container', 'box', 20],
+      ]);
+
+      const expectedMatrix = [
+        [1, 0.04, 0.002, 500, 0.5],
+        [25, 1, 0.05, 12500, 12.5],
+        [500, 20, 1, 250000, 250],
+        [0.002, 0.00008, 0.000004, 1, 0.001],
+        [2, 0.08, 0.004, 1000, 1],
+      ];
+
+      const matrix = units.getMatrix('grid');
+
+      expect(matrix.indices).to.be.deep.equal(unitNames);
+      expect(matrix.grid).to.be.deep.equal(expectedMatrix);
+    });
+
+    it('should calculate conversion grid matrix with Infinity values', () => {
+      const unitNames = ['1', 'box', 'g', 'kg'];
+      const units = new Units(unitNames, [
+        ['kg', 'g', 1000],
+        ['box', '1', 25],
+      ]);
+
+      const expectedMatrix = [
+        [1, 0.04, Infinity, Infinity],
+        [25, 1, Infinity, Infinity],
+        [Infinity, Infinity, 1, 0.001],
+        [Infinity, Infinity, 1000, 1],
+      ];
+
+      const matrix = units.getMatrix('grid');
+
+      expect(matrix.indices).to.be.deep.equal(unitNames);
+      expect(matrix.grid).to.be.deep.equal(expectedMatrix);
+    });
+
+    it('should calculate conversion array matrix', () => {
+      const units = new Units(['1', 'box', 'container', 'g', 'kg'], [
+        ['kg', 'g', 1000],
+        ['1', 'g', 500],
+        ['box', '1', 25],
+        ['container', 'box', 20],
+      ]);
+
+      const expectedMatrix = [
+        ['1', '1', 1],
+        ['1', 'box', 0.04],
+        ['1', 'container', 0.002],
+        ['1', 'g', 500],
+        ['1', 'kg', 0.5],
+        ['box', '1', 25],
+        ['box', 'box', 1],
+        ['box', 'container', 0.05],
+        ['box', 'g', 12500],
+        ['box', 'kg', 12.5],
+        ['container', '1', 500],
+        ['container', 'box', 20],
+        ['container', 'container', 1],
+        ['container', 'g', 250000],
+        ['container', 'kg', 250],
+        ['g', '1', 0.002],
+        ['g', 'box', 0.00008],
+        ['g', 'container', 0.000004],
+        ['g', 'g', 1],
+        ['g', 'kg', 0.001],
+        ['kg', '1', 2],
+        ['kg', 'box', 0.08],
+        ['kg', 'container', 0.004],
+        ['kg', 'g', 1000],
+        ['kg', 'kg', 1],
+      ];
+
+      const matrix = units.getMatrix('array');
+
+      expect(matrix).to.be.deep.equal(expectedMatrix);
+    });
+
+  });
+
+});
---
+++ packages/pretur.units/src/main.ts
@@ -0,0 +1,185 @@
+export type Conversion<U extends string> = [U, U, number];
+
+export type ConversionMatrix<U extends string> = {
+  [S in U]: {[D in U]: number; };
+};
+
+export interface GridMatrix<U extends string> {
+  indices: U[];
+  grid: number[][];
+}
+
+export interface Convertor {
+  (value: number): number;
+}
+
+export default class Units<U extends string> {
+  private units: U[] = [];
+  private matrix: ConversionMatrix<U> = <any>{};
+
+  constructor(units: U[], conversions?: Conversion<U>[]) {
+    units.forEach(unit => {
+      if (this.units.indexOf(unit) === -1) {
+        this.units.push(unit);
+        this.matrix[unit] = <any>{};
+        this.matrix[unit][unit] = 1;
+      }
+    });
+
+    if (Array.isArray(conversions)) {
+      conversions.filter(Array.isArray).forEach(conversion => this.addConversion(conversion));
+    }
+  }
+
+  private addConversion([source, destination, ratio]: Conversion<U>) {
+    if (source === destination) {
+      throw new Error(`source and destination cannot be same`);
+    }
+    if (this.units.indexOf(source) === -1) {
+      throw new Error(`source unit "${source}" is not a valid unit`);
+    }
+    if (this.units.indexOf(destination) === -1) {
+      throw new Error(`destination unit "${destination}" is not a valid unit`);
+    }
+    if (typeof ratio !== 'number') {
+      throw new Error(`Ratio ${ratio} is not a number`);
+    }
+    if (ratio === 0) {
+      throw new Error(`Ratio cannot be zero`);
+    }
+
+    this.matrix[source][destination] = ratio;
+    this.matrix[destination][source] = Math.pow(ratio, -1);
+
+  }
+
+  private calculateCoefficient(source: U, destination: U): number {
+    const stack: string[] = [];
+    stack.push(source);
+
+    // Faster than linear search
+    const visited: { [unit: string]: boolean } = {};
+
+    // DFS visit
+    while (stack.length > 0) {
+      const current = stack.pop()!;
+      if (!visited[current]) {
+        visited[current] = true;
+
+        Object.keys(this.matrix[current]).forEach(next => {
+
+          stack.push(next);
+
+          // Populate the matrix if possible
+          if (!this.matrix[source][next]) {
+            if (this.matrix[source][current] && this.matrix[current][next]) {
+              const ratio = this.matrix[source][current] * this.matrix[current][next];
+              this.matrix[source][next] = ratio;
+              this.matrix[next][source] = Math.pow(ratio, -1);
+            }
+          }
+
+        });
+      }
+    }
+
+    if (this.matrix[source][destination]) {
+      return this.matrix[source][destination];
+    }
+
+    // Source has no path to destination.
+    this.matrix[source][destination] = Infinity;
+    this.matrix[destination][source] = Infinity;
+
+    // None of the visited units can reach destination
+    // if source has no path to destination.
+    Object.keys(visited).forEach(key => {
+      this.matrix[key][destination] = Infinity;
+      this.matrix[destination][key] = Infinity;
+    });
+
+    return Infinity;
+  }
+
+  public getRatio(source: U, destination: U): number {
+    if (this.units.indexOf(source) === -1) {
+      throw new Error(`source unit "${source}" is not a valid unit`);
+    }
+    if (this.units.indexOf(destination) === -1) {
+      throw new Error(`destination unit "${destination}" is not a valid unit`);
+    }
+
+    if (this.matrix[source][destination]) {
+      return this.matrix[source][destination];
+    }
+
+    return this.calculateCoefficient(source, destination);
+  }
+
+  public convert(source: U, destination: U, value: number): number {
+    const ratio = this.getRatio(source, destination);
+    return ratio * value;
+  }
+
+  public createConverter(source: U, destination: U): Convertor | undefined {
+    const ratio = this.getRatio(source, destination);
+    if (ratio === Infinity) {
+      return;
+    }
+    return value => ratio * value;
+  }
+
+  public getMatrix(type: 'nested'): ConversionMatrix<U>;
+  public getMatrix(type: 'grid'): GridMatrix<U>;
+  public getMatrix(type: 'array'): Conversion<U>[];
+  public getMatrix(
+    type: 'nested' | 'grid' | 'array',
+  ): ConversionMatrix<U> | GridMatrix<U> | Conversion<U>[] {
+    switch (type) {
+      case 'nested':
+        const nestedMatrix: ConversionMatrix<U> = <any>{};
+
+        this.units.forEach(source => {
+          nestedMatrix[source] = <any>{};
+
+          this.units.forEach(destination => {
+            nestedMatrix[source][destination] = this.matrix[source][destination]
+              ? this.matrix[source][destination]
+              : this.calculateCoefficient(source, destination);
+          });
+
+        });
+
+        return nestedMatrix;
+      case 'grid':
+        const gridMatrix: GridMatrix<U> = { grid: [], indices: this.units.slice() };
+
+        this.units.forEach((source, i) => {
+          gridMatrix.grid[i] = [];
+
+          this.units.forEach((destination, j) => {
+            gridMatrix.grid[i][j] = this.matrix[source][destination]
+              ? this.matrix[source][destination]
+              : this.calculateCoefficient(source, destination);
+          });
+
+        });
+
+        return gridMatrix;
+      case 'array':
+        const array: Conversion<U>[] = [];
+
+        this.units.forEach(source => {
+          this.units.forEach(destination => {
+            const ratio = this.matrix[source][destination]
+              ? this.matrix[source][destination]
+              : this.calculateCoefficient(source, destination);
+            array.push([source, destination, ratio]);
+          });
+
+        });
+
+        return array;
+    }
+  }
+}
