9369572dc8a8bb58e927ec2e57ccb7bf8303919e
Ali Taheri Moghaddar <ali.taheri.m@gmail.com>
2017-05-08 12:19:15 +0430

    Refactor clay actions

--- packages/pretur.clay/src/actions.ts
+++ packages/pretur.clay/src/actions.ts
@@ -28,47 +28,47 @@ export interface RefreshPayload {
   count: any;
 }
 
-export const CLAY_CLEAR: TargetedActionDescriptor<void, void>
-  = createTargetedActionDescriptor<void, void>('CLAY_CLEAR');
+export const CLAY_CLEAR: TargetedActionDescriptor
+  = createTargetedActionDescriptor('CLAY_CLEAR');
 
-export const CLAY_REPLACE: TargetedActionDescriptor<any, void>
-  = createTargetedActionDescriptor<any, void>('CLAY_REPLACE');
+export const CLAY_REPLACE: TargetedActionDescriptor<any>
+  = createTargetedActionDescriptor<any>('CLAY_REPLACE');
 
-export const CLAY_SET_VALUE: TargetedActionDescriptor<SetValuePayload, void>
-  = createTargetedActionDescriptor<SetValuePayload, void>('CLAY_SET_VALUE');
+export const CLAY_SET_VALUE: TargetedActionDescriptor<SetValuePayload>
+  = createTargetedActionDescriptor<SetValuePayload>('CLAY_SET_VALUE');
 
-export const CLAY_SET_ERROR: TargetedActionDescriptor<ValidationError, void>
-  = createTargetedActionDescriptor<ValidationError, void>('CLAY_SET_ERROR');
+export const CLAY_SET_ERROR: TargetedActionDescriptor<ValidationError>
+  = createTargetedActionDescriptor<ValidationError>('CLAY_SET_ERROR');
 
-export const CLAY_SET_STATE: TargetedActionDescriptor<State, void>
-  = createTargetedActionDescriptor<State, void>('CLAY_SET_STATE');
+export const CLAY_SET_STATE: TargetedActionDescriptor<State>
+  = createTargetedActionDescriptor<State>('CLAY_SET_STATE');
 
-export const CLAY_SET_FIELD: TargetedActionDescriptor<SetFieldPayload, void>
-  = createTargetedActionDescriptor<SetFieldPayload, void>('CLAY_SET_FIELD');
+export const CLAY_SET_FIELD: TargetedActionDescriptor<SetFieldPayload>
+  = createTargetedActionDescriptor<SetFieldPayload>('CLAY_SET_FIELD');
 
-export const CLAY_ADD: TargetedActionDescriptor<any, void>
-  = createTargetedActionDescriptor<any, void>('CLAY_ADD');
+export const CLAY_ADD: TargetedActionDescriptor<any>
+  = createTargetedActionDescriptor<any>('CLAY_ADD');
 
-export const CLAY_REMOVE: TargetedActionDescriptor<number, void>
-  = createTargetedActionDescriptor<number, void>('CLAY_REMOVE');
+export const CLAY_REMOVE: TargetedActionDescriptor<number>
+  = createTargetedActionDescriptor<number>('CLAY_REMOVE');
 
-export const CLAY_REFRESH: TargetedActionDescriptor<RefreshPayload, void>
-  = createTargetedActionDescriptor<RefreshPayload, void>('CLAY_REFRESH');
+export const CLAY_REFRESH: TargetedActionDescriptor<RefreshPayload>
+  = createTargetedActionDescriptor<RefreshPayload>('CLAY_REFRESH');
 
-export const CLAY_SET_QUERY_ATTRIBUTES: TargetedActionDescriptor<string[], void>
-  = createTargetedActionDescriptor<string[], void>('CLAY_SET_QUERY_ATTRIBUTES');
+export const CLAY_SET_QUERY_ATTRIBUTES: TargetedActionDescriptor<string[]>
+  = createTargetedActionDescriptor<string[]>('CLAY_SET_QUERY_ATTRIBUTES');
 
-export const CLAY_SET_QUERY_FILTERS: TargetedActionDescriptor<SetFiltersPayload, void>
-  = createTargetedActionDescriptor<SetFiltersPayload, void>('CLAY_SET_QUERY_FILTERS');
+export const CLAY_SET_QUERY_FILTERS: TargetedActionDescriptor<SetFiltersPayload>
+  = createTargetedActionDescriptor<SetFiltersPayload>('CLAY_SET_QUERY_FILTERS');
 
-export const CLAY_SET_QUERY_PAGINATION: TargetedActionDescriptor<QueryPagination, void>
-  = createTargetedActionDescriptor<QueryPagination, void>('CLAY_SET_QUERY_PAGINATION');
+export const CLAY_SET_QUERY_PAGINATION: TargetedActionDescriptor<QueryPagination>
+  = createTargetedActionDescriptor<QueryPagination>('CLAY_SET_QUERY_PAGINATION');
 
-export const CLAY_SET_QUERY_ORDER: TargetedActionDescriptor<QueryOrder, void>
-  = createTargetedActionDescriptor<QueryOrder, void>('CLAY_SET_QUERY_ORDER');
+export const CLAY_SET_QUERY_ORDER: TargetedActionDescriptor<QueryOrder>
+  = createTargetedActionDescriptor<QueryOrder>('CLAY_SET_QUERY_ORDER');
 
-export const CLAY_SET_QUERY_EXTRA: TargetedActionDescriptor<SetExtraPayload, void>
-  = createTargetedActionDescriptor<SetExtraPayload, void>('CLAY_SET_QUERY_EXTRA');
+export const CLAY_SET_QUERY_EXTRA: TargetedActionDescriptor<SetExtraPayload>
+  = createTargetedActionDescriptor<SetExtraPayload>('CLAY_SET_QUERY_EXTRA');
 
-export const CLAY_SET_QUERIEIR_COUNT: TargetedActionDescriptor<number, void>
-  = createTargetedActionDescriptor<number, void>('CLAY_SET_QUERIEIR_COUNT');
+export const CLAY_SET_QUERIEIR_COUNT: TargetedActionDescriptor<number>
+  = createTargetedActionDescriptor<number>('CLAY_SET_QUERIEIR_COUNT');
