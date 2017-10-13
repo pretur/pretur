635b7e396b877f2182f5ed359d7531341b7441d5
Ali Taheri <ali.taheri.m@gmail.com>
2017-10-13 19:02:35 +0330

    Update dependencies

--- packages/pretur.redux/src/node.spec.ts
+++ packages/pretur.redux/src/node.spec.ts
@@ -2,7 +2,7 @@
 
 import { expect } from 'chai';
 import { Action } from './action';
-import { buildNode } from './reducer';
+import { buildNode } from './node';
 
 class ReduceMe {
   public changing: boolean;
